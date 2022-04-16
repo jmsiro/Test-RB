d1dd9ed5c654886c22e4eee50b4a48f18c35fc79
jmsiro <jmsiro@gmail.com>
2022-04-16 20:13:37 -0300

    Test:Matriz 2.0

--- datos.py
+++ datos.py
@@ -1,18 +1,29 @@
 from openpyxl import Workbook, load_workbook
 
-libro = load_workbook(filename='Base/Base Seguimiento Observ Auditoría al_30042021.xlsx', read_only=True)
+# Abro libro. Los flags indican que es modo lectura y que muesta los resultados de las formulas del excel.
+libro = load_workbook(filename='Base/Base Seguimiento Observ Auditoría al_30042021.xlsx', read_only=True, data_only=True)
 
-hoja = libro.active
+#En este caso que es una sola hoja tomo la hoja activa, pero en caso que haya mas de una deberia seleccionarla utilizando el nombre de la misma como si fuera un diccionario.
+hoja = libro['Hoja1']
 
 matriz = []
+matriz_atr = []
 datos = {}
 
-for c in range(1,20):
-    if hoja.cell(row=1, column=c).value == None:
+# Loop 1 = Recorre filas
+# Loop 2 = Recorre Columnas
+# Se crea una Lista de Diccionarios donde las 'Keys' son los titulos de las columnas y el 'Value' el valor de esa columna para cada fila.
+for r in range(2,10):
+    if hoja.cell(row=r, column=1).value == None:
         break
     else:
-        for r in range(2,10):
-            datos[hoja.cell(row=1, column=c).value] = hoja.cell(row=r, column=c).value
-        matriz.append(datos)
-
-print(matriz[2])
+        for c in range(1,20):
+            if hoja.cell(row=1, column=c).value == None:
+                matriz.append(datos.copy()) # Se genera una copia del diccionario y se incopora a la lista
+                datos.clear() # Se limpia del diccionario original para la proxima vuelta del loop
+                break
+            else:
+                datos[hoja.cell(row=1, column=c).value] = hoja.cell(row=r, column=c).value
+                
+for i in range(0,len(matriz)):
+    print(matriz[i])
